2e3733b5f432abde17df0918b1761eba33e683ef
polariisd <152268714+polariisd@users.noreply.github.com>
2026-01-16 10:14:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080; // On garde le port 8080 comme dans l'atelier
-
+const port = 8080; 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
